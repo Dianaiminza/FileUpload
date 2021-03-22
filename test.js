aa08d6d63428ff8e4bb0a315b6a8a7bb98f7512c
DIANA IMINZA <51244262+Dianaiminza@users.noreply.github.com>
2021-03-22 12:24:03 +0300

    Update test.js

--- test.js
+++ test.js
@@ -4,26 +4,27 @@ const request = supertest(app);
 const expect = require("chai").expect;
 
   describe("POST /multiple", function() {
-   it("it should has status code 200 if the file is uploaded", function(done) {
-    supertest(app)
-    .post('/multiple')
-      .attach("file", "C:\\Users\\Captain\\Pictures\\Saved Pictures\\health.jpg")
-      .attach("file", "C:\\Users\\Captain\\Pictures\\Saved Pictures\\coffee.jpg")
-      // .attach("file", "C:\Users\Captain\Documents\CV.docx")
+    it("it should has status code 200 if the file is uploaded", function(done) {
+     supertest(app)
+     .post('/multiple')
+      .attach("file", "C:\\Users\\Captain\\Desktop\\Image\\testfiles\\health.jpg")
+      .attach("file", "C:\\Users\\Captain\\Desktop\\Image\\testfiles\\image.png")
       .expect(200)
       .end(function(err, res){
         if (err) done(err);
         done();
       });
   });
-}); 
+});
 
 describe("POST /multiple", function() {
-  it("it should has status code 200 if the file exist", function(done) {
+  it("it should has status code 200 if the files exist", function(done) {
    supertest(app)
    .post('/multiple')
-      .attach("file", "C:\\Users\\Captain\\Pictures\\Saved Pictures\\health.jpg")
-      .attach("file", "C:\\Users\\Captain\\Pictures\\Saved Pictures\\coffee.jpg")
+      // .attach("file", "C:\\Users\\Captain\\Pictures\\Saved Pictures\\health.jpg")
+      // .attach("file", "C:\\Users\\Captain\\Pictures\\Saved Pictures\\coffee.jpg")
+      .attach("file", "C:\\Users\\Captain\\Desktop\\Image\\testfiles\\health.jpg")
+      .attach("file", "C:\\Users\\Captain\\Desktop\\Image\\testfiles\\image.png")
       .expect(200)
       .end(function(err, res){
         if (err) done(err);
@@ -32,11 +33,11 @@ describe("POST /multiple", function() {
   });
 });
    describe("POST /multiple", function() {
-   it("it should has status code 200 if the file is of type  png,jpg,jpeg", function(done) {
+   it("it should has status code 200 if the files are of type  png,jpg,jpeg", function(done) {
     supertest(app)
     .post('/multiple')
-      .attach("file", "C:\\Users\\Captain\\Pictures\\Saved Pictures\\health.jpg")
-      .attach("file", "C:\\Users\\Captain\\Pictures\\Saved Pictures\\coffee.jpg")
+    .attach("file", "C:\\Users\\Captain\\Desktop\\Image\\testfiles\\health.jpg")
+    .attach("file", "C:\\Users\\Captain\\Desktop\\Image\\testfiles\\image.png")
       // .attach("file", "C:\Users\Captain\Documents\CV.docx")
       .expect(200)
       .end(function(err, res){
